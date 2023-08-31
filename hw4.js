d52c8c6dddad635b1552624c2f62e1e0c03dbe55
nandityasf <you@example.com>
2023-08-31 22:03:22 +0700

    komentar

---
+++ hw4.js
@@ -0,0 +1,19 @@
+let nilairandom = [];
+for (let i = 1; i <= 100; i++) {
+  let nilai = Math.round(Math.random() * 50);
+  nilairandom.push(nilai);
+}
+console.log([nilairandom.length])
+console.log(nilairandom);
+
+if (nilairandom % 2 == 0){
+  console.log(`ini adalah bilangan genap`)
+}
+
+// let nilairandom = [];
+ 
+//     for (let i = 1; i <= 100; i++) {
+//        nilairandom.push(i);
+//     }
+//     console.log([nilairandom.length])
+//     console.log(nilairandom)
