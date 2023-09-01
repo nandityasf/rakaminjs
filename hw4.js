ffe24bb655ea78264402683f1dff93e2dee30149
nandityasf <you@example.com>
2023-09-01 16:04:41 +0700

    homework4

--- hw4.js
+++ hw4.js
@@ -1,20 +1,27 @@
+//Buatlah 100 nilai random (1 sampai 50) pada 1 array
 let nilairandom = [];
 for (let i = 1; i <= 100; i++) {
-  let nilai = Math.round(Math.random() * 50);
+  let nilai = Math.floor(Math.random() * 50);
   nilairandom.push(nilai);
 }
 console.log([nilairandom.length])
 console.log(nilairandom);
 
-// let nilairandom = []
-// if (nilairandom % 2 == 0){
-//   {console.log("genap") }else { console.log("ganjil")}
-// }
+// pecah bilangan ganjil genap
+const angkaGenap = []
+const angkaGanjil = []
+for(let i=0; i < nilairandom.length; i++){
+  if(i % 2 === 0){
+    angkaGenap.push(nilairandom[i])
+  }
+  else{
+    angkaGanjil.push(nilairandom[i])
+  }
+}
+console.log("angka genap:", angkaGenap)
+console.log([angkaGenap.length])
+console.log("angka ganjil:", angkaGanjil)
+console.log([angkaGanjil.length])
 
-// let nilairandom = [];
- 
-//     for (let i = 1; i <= 100; i++) {
-//        nilairandom.push(i);
-//     }
-//     console.log([nilairandom.length])
-//     console.log(nilairandom)
+//min, max, total, rata-rata
+// 
